65bcc39cba99d08664c8cd4c414378ae1d1fc5d9
notacat222 <ogawahan831@gmail.com>
2025-05-15 19:37:56 +1000

    Update sketch_250508b.js

--- sketch_250508b.js
+++ sketch_250508b.js
@@ -1,11 +1,12 @@
 var sound, analyzer, volume, string, myButton;
 let img;
-let imgWidth = 200;
-let imgHeight = 60;
-
+let imgWidth = 70;
+let imgHeight = 70;
+let mappedVolume=100;
+let go=false;
 function preload(){
- sound=loadSound("workbook.mp3");
- img = loadImage("ME.png")
+ sound=loadSound("data/workbook.mp3");
+ img = loadImage("data/ME.png")
 }
 function setup() {
   createCanvas(windowWidth,windowHeight);
@@ -14,14 +15,17 @@ function setup() {
   analyzer= new p5.Amplitude();
   myButton=createButton("CLICK ME");
   myButton.position(random(width),random(height));
+  noLoop();
 }
 
 
 function draw() {
   background(0);
   volume=analyzer.getLevel();
-  mappedVol= map(volume, 0, 1.0, 0, width);
-  image (img, mouseX,mouseY, mappedVol, imgWidth, imgHeight);
+  mappedVol= map(volume, 0, 1.0, 0, width/6);
+  if (go==true){
+  image (img, mouseX,mouseY, mappedVol, mappedVol);
+  }
   push();
   translate(width/2,height/2);
   rotate(mappedVol);
@@ -32,12 +36,14 @@ function draw() {
 
 }
 function mousePressed(){
+  go=true;
    getAudioContext().resume();
  analyzer= new p5.Amplitude();
  analyzer.setInput(sound);
  loop();
   if(sound.isPlaying()==true){
  sound.stop()
+ go=false;
  sound.noLoop();
 }
 else {
