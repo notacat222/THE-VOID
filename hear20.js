95cfce5ca52a7a0b4f580bf0b09745bf788b984d
notacat222 <ogawahan831@gmail.com>
2025-05-15 19:49:08 +1000

    Add files via upload

---
+++ hear20.js
@@ -0,0 +1,60 @@
+var sound, analyzer, volume, string, myButton;
+let img;
+let imgWidth = 60;
+let imgHeight = 60;
+let mappedVolume=100;
+let go=false;
+function preload(){
+ sound=loadSound("data/workbook.mp3");
+ img = loadImage("data/ME.png")
+}
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background(0);
+  getAudioContext().suspend();
+  analyzer= new p5.Amplitude();
+  myButton=createButton("CLICK ME");
+  myButton.position(random(width),random(height));
+  noLoop();
+}
+
+
+function draw() {
+  background(0);
+  volume=analyzer.getLevel();
+  mappedVol= map(volume, 0, 1.0, 0, width/6);
+  if (go==true){
+  image (img, mouseX,mouseY, mappedVol, mappedVol);
+  }
+  push();
+  translate(width/2,height/2);
+  rotate(mappedVol);
+    textSize(mappedVol);
+    text(string,0,0);
+  pop();
+  console.log(volume + '|'+mappedVol);
+
+}
+function mousePressed(){
+  go=true;
+   getAudioContext().resume();
+ analyzer= new p5.Amplitude();
+ analyzer.setInput(sound);
+ loop();
+  if(sound.isPlaying()==true){
+ sound.stop()
+ go=false;
+ sound.noLoop();
+}
+else {
+  sound.play();
+  sound.loop();
+  }
+}
+
+function keyPressed(){
+  if (key=='b'){
+    fill("blue");
+  }
+  string=string+key;
+}
